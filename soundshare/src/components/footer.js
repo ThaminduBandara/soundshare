a9efebda6534362421ea9e1f09c0a57cbfbf5dff
thamindubandara <bandaradambewela1960@gmail.com>
2025-08-16 15:21:33 +0530

    footer ui is done

---
+++ soundshare/src/components/footer.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import './footer.css'
+import fb from '../assests/fb.svg'
+import ig from '../assests/SVG.svg'
+import x from '../assests/twitter.svg'
+import yt from '../assests/youtube.svg'
+
+export default function Footer() {
+  return (
+    <div className='footer-container'>
+
+        <div className='f-1'>
+            <p className='f-name'>SoundShare</p>
+            <p className='caption'>Discover and share your favorite sounds</p>
+        </div>
+
+        <div className='f-2'>
+            <p>Terms of Service</p>
+            <p>Privacy Policy</p>
+         <p>Cookie Policy</p>
+        </div>
+
+        <div className='f-3'>
+
+            <div className='line'></div>
+            <p>© 2024 SoundShare. All rights reserved.</p>
+        </div>
+
+        <div className='f-4'>
+             <p className='help-center'>Help Center</p>
+             <p>Community Guidelines</p>
+             <p>Contact Us</p>
+        </div>
+
+        <div className='f-5'>
+              <img src={fb} alt='fb'/>
+              <img src={ig} alt='ig'/>
+              <img src={x} alt='x'/>
+              <img src={yt} alt='yt'/>
+        </div>
+       
+       
+         
+        
+
+
+    </div>
+  )
+}
---
+++ soundshare/src/pages/login.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import './login.css';
+
+export default function Login()
+{
+  return (
+    <div className='login-container'>
+
+      <div className='login-middle-container'>
+
+      <div className='logo'>
+        <svg xmlns="http://www.w3.org/2000/svg" width="97" height="100" viewBox="0 0 97 100" fill="none">
+        <path d="M88.7475 1L28.3781 19.3246C27.1207 19.7306 26.1018 20.4795 25.3216 21.5714C24.5413 22.6633 24.1508 23.8866 24.15 25.2414V75.937C23.1615 75.6904 22.1624 75.5035 21.1528 75.3764C20.1431 75.2492 19.1297 75.1827 18.1125 75.1768C8.11289 75.1768 0 80.7349 0 87.5884C0 94.4419 8.11289 100 18.1125 100C28.1121 100 36.225 94.4419 36.225 87.5884V42.2705L84.525 27.7257V63.5273C83.5366 63.2799 82.5376 63.0924 81.5279 62.9649C80.5182 62.8375 79.5048 62.7709 78.4875 62.7652C68.4879 62.7652 60.375 68.3233 60.375 75.1768C60.375 82.0303 68.4879 87.5884 78.4875 87.5884C88.4871 87.5884 96.6 82.0323 96.6 75.1768V6.91297C96.5981 4.81763 95.7749 3.15083 94.1305 1.91255C92.486 0.674274 90.6917 0.37009 88.7475 1Z" fill="#721111"/>
+        </svg>
+        <p className='logo-name'>SOUND SHARE</p>
+      </div>
+      
+
+
+        <div className='l-header'>
+          <p className='welcome'>Welcome back</p>
+          <p className='continue'>Sign in to continue</p>
+        </div>
+        
+        <div className='login-inputs'>
+          <input type="text" className='l-email' placeholder='Email'/>
+          <input type="password" className='l-password' placeholder='Password'/>
+          <button className='login-button'>Sign In</button>
+        </div>
+
+        <div className='login-footer'>
+          <p className='forgot-password'>Forgot Password?</p>
+          <p className='dont-have'>Don't have an account? <span className='l-signup'>Sign Up</span></p>
+        </div>
+
+        
+
+      </div>
+
+    </div>
+  )
+}
